d91acdc8f500eaad1c261495d4c3dbe5fd017a14
RakshitRabugotra <rakshit.rabugotra000@gmail.com>
2025-05-29 00:57:15 +0530

    fix: add dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { JSONFilePreset } from 'lowdb/node';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
